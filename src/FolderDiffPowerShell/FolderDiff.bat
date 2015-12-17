546f3c77f9ffeaf5876568442ab914edbb9dca61
sietsevdschoot <sietsevdschoot@gmail.com>
2015-12-17 10:16:14 +0100

    Make file more readable by shortening lines.

--- src/FolderDiffPowerShell/FolderDiff.bat
+++ src/FolderDiffPowerShell/FolderDiff.bat
@@ -8,4 +8,8 @@ SET SupportLongFilenames=%3
 IF [%3]==[] SET SupportLongFilenames=$False
 
 ECHO.   
-PowerShell.exe -NoProfile -ExecutionPolicy ByPass -Command "%CurrentPath%FolderDiff.ps1 %ReferenceFolder% %DifferenceFolder% %SupportLongFilenames%  | ForEach { $_.FullName }"
+PowerShell.exe ^
+      -NoProfile ^
+      -ExecutionPolicy ByPass ^
+      -Command ^
+      "%CurrentPath%FolderDiff.ps1 %ReferenceFolder% %DifferenceFolder% %SupportLongFilenames%  | ForEach { $_.FullName }"
